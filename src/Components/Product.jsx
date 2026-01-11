0cf8b39df33623314ec8851ea7494c343b3ce9dd
Rahul84825 <activegamer789@gmail.com>
2026-01-11 17:04:24 +0530

    Page structure created

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import useFetch from "../Hooks/useFetch";
 
-function Product() {
+function Products() {
+  const { data, loading, error } = useFetch(
+    // yet have to add the url for api; 
+  );
+
+  const items = Array.from({ length: 50 }, (_, i) => i);
+
   return (
-    <div>Product</div>
-  )
+    <div className="min-h-screen bg-black">
+      <div className="fixed top-0 left-0 w-full bg-black z-50">
+        <h1 className="text-white text-3xl font-semibold text-center py-4">
+          Photos
+        </h1>
+      </div>
+
+      <div className="px-10 pt-24 pb-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+          {items.map((item, index) => (
+            <div
+              key={index}
+              className="border border-gray-500 p-4 text-center"
+            >
+              <div className="w-full aspect-square mb-3 bg-gray-700"></div>
+
+              <p className="text-gray-300 text-sm">
+                Lorem ipsum dolor sit amet.
+              </p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Product
+export default Products;
--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import { useState, useEffect } from "react";
 
-function useFetch() {
-  return (
-    <div>useFetch</div>
-  )
+function useFetch(url) {
+
+    const[data, setData] = useState([]);
+    async function fetchData(){
+        try{
+            const res = await fetch(url);
+
+            const result = await res.json();
+            setData(result);
+        }catch(err){
+            console.log("error", err);
+        }
+    }
+
+    useEffect(() => {
+        fetchData();
+    }, [])
+
+
+  return { data };
 }
 
 export default useFetch
