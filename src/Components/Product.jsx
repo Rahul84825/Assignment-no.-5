e3a71d583718e6d69018d131c9371b45a1d5a30c
Rahul84825 <activegamer789@gmail.com>
2026-01-11 19:52:10 +0530

    Solved the Refrence Error

--- src/Components/Product.jsx
+++ src/Components/Product.jsx
@@ -1,6 +1,4 @@
-import React from "react";
 import useFetch from "../Hooks/useFetch";
-
 
 function Products() {
 
@@ -15,7 +13,7 @@ function Products() {
       title: "Color Lorem ipsum dolor sit amet.",
     };
   });
-  
+
   return (
     <div className="min-h-screen bg-black">
       <div className="fixed top-0 left-0 w-full bg-black z-50">
@@ -26,18 +24,23 @@ function Products() {
 
       <div className="px-10 pt-24 pb-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
-          {items.map((item, index) => (
-            <div
-              key={index}
-              className="border border-gray-500 p-4 text-center"
-            >
-              <div className="w-full aspect-square mb-3 bg-gray-700"></div>
+          {colors.map((item, index) => {
+            return (
+              <div
+                key={index}
+                className="border border-gray-500 p-4 text-center"
+              >
+                <div
+                  className="w-full aspect-square mb-3"
+                  style={{ backgroundColor: item.color }}
+                ></div>
 
-              <p className="text-gray-300 text-sm">
-                Lorem ipsum dolor sit amet.
-              </p>
-            </div>
-          ))}
+                <p className="text-gray-300 text-sm">
+                  {item.title}
+                </p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
