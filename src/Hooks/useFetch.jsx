4b35b34ececa8a9ee3f3ea27c08b2e2832463f27
Rahul84825 <activegamer789@gmail.com>
2026-01-13 08:28:28 +0530

    Added the loading and error in useFetch

--- src/Hooks/useFetch.jsx
+++ src/Hooks/useFetch.jsx
@@ -1,24 +1,30 @@
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function useFetch(url) {
-  const [data, setData] = useState([]);
+  const[data, setData] = useState([]);
+  const[loading, setLoading] = useState(false);
+  const[error, setError] = useState(null);
 
-  async function fetchData() {
-    try {
+  async function fetchData(url){
+    setLoading(true);
+    setError(null);
+
+    try{
       const res = await fetch(url);
+
+      if(!res.ok){
+        throw new Error("Something went wrong!");
+      }
       const result = await res.json();
       setData(result);
-    } catch (err) {
-      console.log("error", err);
+    }catch(err){
+      setError(err.message);
+      console.log(err);
     }
+    setLoading(false);
   }
 
-  useEffect(() => {
-    
-    fetchData();
-  }, []);
-
-  return { data };
+  return {data, loading, error};
 }
 
 export default useFetch;
